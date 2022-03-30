a5a53872c3713bf62eb11f60bd425e5742241d10
ke.ma <ke.ma@thoughtworks.com>
2022-03-30 17:18:49 +0800

    [zijie & make] feat: add ofNullable demo to Stream API class

--- src/Java9/Java9StreamAPI.java
+++ src/Java9/Java9StreamAPI.java
@@ -7,8 +7,18 @@ public class Java9StreamAPI {
     public static void main(String[] args) {
         List<Integer> list1 = List.of(1, 2, 3, 4, 5);
         List<Integer> list2 = List.of(5, 4, 3, 2, 1);
+        List<Integer> list3 = null;
+        //take while demo
         list1.stream().takeWhile(s -> s < 4).forEach(System.out::println);
+
+        //drop while demo
         list2.stream().dropWhile(s -> s >= 4).forEach(System.out::println);
-        Stream.iterate('a', x-> x < 200, x -> x++).forEach(System.out::println);
+
+        //iterate demo
+        Stream.iterate(3, x-> x < 10, x -> x+3).forEach(System.out::println);
+
+        //ofNullable demo
+        long count = Stream.ofNullable(list3).count();
+        System.out.println(count);
     }
 }
