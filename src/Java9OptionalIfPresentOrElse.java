f933515fa54014dbb6a79bc9a552812074a94336
Zijie Pan <zijie.pan@hyrecar.com>
2022-03-30 14:59:52 +0800

    [Zijie & Make] feat: Add Java 9 optional ifPresentOrElse method

---
+++ src/Java9OptionalIfPresentOrElse.java
@@ -0,0 +1,15 @@
+import java.util.Optional;
+
+public class Java9OptionalIfPresentOrElse {
+    public static void main(String[] args) {
+        Optional<Integer> optional = Optional.of(1);
+
+        optional.ifPresentOrElse( x -> System.out.println("Value: " + x),() ->
+                System.out.println("Not Present."));
+
+        optional = Optional.empty();
+
+        optional.ifPresentOrElse( x -> System.out.println("Value: " + x),() ->
+                System.out.println("Not Present."));
+    }
+}
