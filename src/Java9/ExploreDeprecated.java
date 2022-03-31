0affb79cb5c7fa284267dc9ceea7e6cd414dbe32
Zijie Pan <zijie.pan@hyrecar.com>
2022-03-31 10:49:40 +0800

    [Zijie & Make] feat: Java9 deprecated

---
+++ src/Java9/ExploreDeprecated.java
@@ -0,0 +1,20 @@
+package Java9;
+
+
+public class ExploreDeprecated {
+    private final int radius = 5;
+
+    @Deprecated(since = "9", forRemoval = true)
+    public Double sizeDeprecated(){
+        return  Math.pow(this.radius,2);
+    }
+
+    public Double size(){
+        return  Math.pow(this.radius,2);
+    }
+
+    public static void main(String[] args) {
+        ExploreDeprecated exploreDeprecated = new ExploreDeprecated();
+        System.out.println(exploreDeprecated.sizeDeprecated().equals(exploreDeprecated.size()));
+    }
+}
