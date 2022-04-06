703d514650eb6eebd68337397aad11ff763aa8b4
Zijie Pan <zijie.pan@hyrecar.com>
2022-04-06 10:45:45 +0800

    [Zijie & Make] Refactor: Add Java 10 annotations

--- src/Java10/UnmodifiableCollectionDemo.java
+++ src/Java10/UnmodifiableCollectionDemo.java
@@ -8,6 +8,16 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
+/*
+    java.util.List, java.util.Map and java.util.Set each got a new static
+    method copyOf(Collection).It returns the unmodifiable copy of the
+    given Collection.
+    java.util.stream.Collectors get additional methods to collect a Stream
+    into unmodifiable List, Map or Set.
+    Any attempt to modify such a collection would result in
+    java.lang.UnsupportedOperationExceptionruntime exception.
+* */
+
 public class UnmodifiableCollectionDemo {
 
     List<Integer> someIntList = new ArrayList<>(Arrays.asList(1,2,3));
