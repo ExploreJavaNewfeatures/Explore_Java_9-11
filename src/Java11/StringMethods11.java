092e7b239bcc2b89b30889525ae11826ff036cb9
zpan_rba <zpan@ritchiebros.com>
2022-04-07 10:07:35 +0800

    [Zijie & Make] feat: Add Java 11 string methods

--- src/Java11/StringMethods11.java
+++ src/Java11/StringMethods11.java
@@ -1,5 +1,14 @@
 package Java11;
 
-public class StringMethods11 {
+import java.util.stream.Collectors;
 
+public class StringMethods11 {
+    public static void main(String[] args) {
+        System.out.println("Mark".isBlank());
+        System.out.println(" Mark ".strip());
+        System.out.println(" Mark Ma".stripLeading());
+        System.out.println(" Mark Ma ".stripTrailing());
+        System.out.println(" Mark Ma ".repeat(2));
+        System.out.println(" Mark \r Ma \n".repeat(2).lines().collect(Collectors.toList()));
+    }
 }
