e8b2ef13f7a2d08bed07c78f3a7a606af759a42d
zpan_rba <zpan@ritchiebros.com>
2022-04-07 14:30:42 +0800

    [Zijie & Make] feat: Add mismatch check in Java 12

---
+++ src/Java12/MisMatchCheck.java
@@ -0,0 +1,22 @@
+package Java12;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.junit.Assert.assertEquals;
+
+public class MisMatchCheck extends Throwable{
+    @Test
+    public void givenIdenticalFiles_thenShouldNotFindMismatch() throws IOException {
+        Path filePath1 = Files.createTempFile("file1", ".txt");
+        Path filePath2 = Files.createTempFile("file2", ".txt");
+        Files.writeString(filePath1, "Java 12 Article");
+        Files.writeString(filePath2, "Java 12 Article");
+
+        long mismatch = Files.mismatch(filePath1, filePath2);
+        assertEquals(-1, mismatch);
+    }
+}
