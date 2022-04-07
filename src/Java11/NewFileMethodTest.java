f405cd9ec06b1898a6ee3660ef1ce7a059b281fa
kema <kema@ritchiebros.com>
2022-04-07 15:03:05 +0800

    [zijie & make] fix: format in new file method demo to java11

--- src/Java11/NewFileMethodTest.java
+++ src/Java11/NewFileMethodTest.java
@@ -1,6 +1,5 @@
 package Java11;
 
-import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
@@ -10,9 +9,9 @@ import java.nio.file.Path;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
 
-public class NewFileMethod {
+public class NewFileMethodTest {
     @Test
-    public void should_equal_to_when_get_string_from_demo_txt() throws IOException {
+    public void should_equal_when_get_string_from_demo_txt() throws IOException {
         Path filePath = Files.writeString(Files.createTempFile(Path.of("./"), "demo", ".txt"), "Sample text");
         String fileContent = Files.readString(filePath);
         assertThat(fileContent, is("Sample text"));
